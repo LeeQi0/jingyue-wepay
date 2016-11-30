75d5aaec56464a97d77a28cd912ef729ce007683
LeeQI <451117877@QQ.COM>
2016-11-30 21:21:59 +0800

    Create getdata.php

---
+++ getdata.php
@@ -0,0 +1,11 @@
+<?php
+// 只需要 TicketId,wheretopay,wheretobuy,transaction_id
+public function getdata($data){
+  $return= array();
+  $return['ticketid']=
+  $return['IDWheretoBuy']=
+  $return['IDWheretoPay']=
+  $return['transaction_id']=
+  
+  return $return;
+}
